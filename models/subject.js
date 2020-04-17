3d38cb1e966dba56895535c567b5ec57f2655307
PetarAsenov <p.a.asenov@gmail.com>
2020-04-17 17:00:45 +0200

    add image url in subject

--- models/subject.js
+++ models/subject.js
@@ -1,7 +1,8 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const subject = sequelize.define('subject', {
-    name: DataTypes.STRING
+    name: DataTypes.STRING,
+    image_Url: DataTypes.TEXT
   }, {});
   subject.associate = function(models) {
     subject.hasMany(models.session, { foreignKey: "subject_id" });
--- seeders/20200416092745-subjects.js
+++ seeders/20200416092745-subjects.js
@@ -7,21 +7,25 @@ module.exports = {
     const subjects = await Promise.all([
       Subject.upsert({
         name: "Maths",
+        image_Url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRqS8I4hGyaKHR0lTlNca90ltJWlE91D2aCxp5Of5Xa57mt0Ty8&usqp=CAU',
         createdAt: new Date(),
         updatedAt: new Date(),
       }),
       Subject.upsert({
         name: "Coding",
+        image_Url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSmGtOwX5zVvIccDNchsWjkTtzAC1F9wf3a2Nh-KkBRs7i0lFQj&usqp=CAU',
         createdAt: new Date(),
         updatedAt: new Date(),
       }),
       Subject.upsert({
         name: "English",
+        image_Url: 'http://www.universum-ks.org/blog/wp-content/uploads/2017/05/English-1-800x434.jpg',
         createdAt: new Date(),
         updatedAt: new Date(),
       }),
       Subject.upsert({
         name: "Dutch",
+        image_Url: 'https://images.pexels.com/photos/2166591/pexels-photo-2166591.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
         createdAt: new Date(),
         updatedAt: new Date(),
       }),
