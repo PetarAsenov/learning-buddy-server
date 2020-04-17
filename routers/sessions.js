b18306aecf798b0d34841424f5857e6279d55f8e
PetarAsenov <p.a.asenov@gmail.com>
2020-04-17 09:06:26 +0200

    make routes for search, delete and update session

--- routers/sessions.js
+++ routers/sessions.js
@@ -1,15 +1,19 @@
 const { Router } = require("express");
+const db = require("../models/");
+const Op = db.Sequelize.Op;
 const Session = require("../models/").session;
 const Subject = require("../models/").subject;
 const User = require("../models/").user;
 const Participant = require("../models/").participant;
 const auth = require("../auth/middleware");
 
-
 const router = new Router();
 
 router.get("/sessions", async (req, res) => {
+  const title = req.query.title;
+  const condition = title ? { title: { [Op.iLike]: `%${title}%` } } : null;
   const sessions = await Session.findAll({
+    where: condition,
     include: [
       { model: User, as: "teacher", attributes: ["name"] },
       { model: Subject, attributes: ["name"] },
@@ -37,6 +41,21 @@ router.get("/session/:id", async (req, res) => {
     ],
   });
   res.status(200).send(sessionDetails);
+});
+
+router.delete("/session/:id", async (req, res) => {
+  const { id } = req.params;
+
+  await Session.destroy({ where: { id: id } });
+  res.send({ message: "Session was deleted" });
+});
+
+router.patch("/session/:id", async (req, res) => {
+  const { id } = req.params;
+  const session = await Session.findByPk(id);
+  const { title, description, start_date, end_date, subject_id } = req.body;
+  await session.update({ title, description, start_date, end_date, subject_id });
+  res.send({ session });
 });
 
 router.post("/sessions", auth, async (req, res) => {
